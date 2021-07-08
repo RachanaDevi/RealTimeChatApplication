715c19ca76219614ce0eb5f1f001bf643b75674e
Rachana Devi <rachanadevi18@gmail.com>
2021-07-08 22:55:46 +0530

    [Rachana] Initial commit

---
+++ src/main/java/com/rachana/chat/realtime_app/RealtimeChatApplication.java
@@ -0,0 +1,13 @@
+package com.rachana.chat.realtime_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatApplication.class, args);
+	}
+
+}
