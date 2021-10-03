77a3c5dba3fb17959806abbe09599000f29ea9d9
Rachana Devi <rachana.devi@thoughtworks.com>
2021-10-03 21:31:10 +0530

    [Rachana] | Adds WebSocket Configurations

--- src/main/java/com/rachana/chat/realtime_app/controller/ChatController.java
+++ src/main/java/com/rachana/chat/realtime_app/controller/ChatController.java
@@ -2,6 +2,9 @@ package com.rachana.chat.realtime_app.controller;
 
 import com.rachana.chat.realtime_app.model.Message;
 import com.rachana.chat.realtime_app.producer.Producer;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +21,12 @@ public class ChatController {
     @PostMapping(value = "/send", consumes = "application/json")
     public void sendMessage(@RequestBody Message message) {
         producer.publish(message);
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/group")
+    public Message broadcastGroupMessage(@Payload Message message) {
+        //sending this message to all the subscribers
+        return message;
     }
 }
---
+++ src/main/java/com/rachana/chat/realtime_app/web_socket/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.rachana.chat.realtime_app.web_socket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // chat client will use this to connect to the server
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic/");
+    }
+}
